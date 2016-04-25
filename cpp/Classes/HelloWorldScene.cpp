1a6f5b936045cf0b284d3fe80776a6edda9d6ca3
tinybearc <tinybearc@gmail.com>
2016-04-25 08:28:31 +0800

    add cpp

--- cpp/Classes/HelloWorldScene.cpp
+++ cpp/Classes/HelloWorldScene.cpp
@@ -1,5 +1,6 @@
 
 #include "HelloWorldScene.h"
+#include "PluginSdkboxAds/PluginSdkboxAds.h"
 
 USING_NS_CC;
 
@@ -7,13 +8,13 @@ Scene* HelloWorld::createScene()
 {
     // 'scene' is an autorelease object
     auto scene = Scene::create();
-
+    
     // 'layer' is an autorelease object
     auto layer = HelloWorld::create();
-
+    
     // add layer as a child to scene
     scene->addChild(layer);
-
+    
     // return the scene
     return scene;
 }
@@ -27,9 +28,9 @@ bool HelloWorld::init()
     {
         return false;
     }
-
+    
     CCLOG("Sample Startup");
-
+    
     // add logo
     auto winsize = Director::getInstance()->getWinSize();
     auto logo = Sprite::create("Logo.png");
@@ -37,7 +38,7 @@ bool HelloWorld::init()
     logo->setPosition(Vec2(logoSize.width / 2,
                            winsize.height - logoSize.height / 2));
     addChild(logo);
-
+    
     // add quit button
     auto label = Label::createWithSystemFont("QUIT", "sans", 32);
     auto quit = MenuItemLabel::create(label, [](Ref*){
@@ -47,30 +48,101 @@ bool HelloWorld::init()
     quit->setPosition(Vec2(winsize.width / 2 - labelSize.width / 2 - 16,
                            -winsize.height / 2 + labelSize.height / 2 + 16));
     addChild(Menu::create(quit, NULL));
-
+    
     // add test menu
     createTestMenu();
-
+    
     return true;
 }
 
 void HelloWorld::createTestMenu()
 {
+    sdkbox::PluginSdkboxAds::setListener(this);
+    
     auto menu = Menu::create();
-
-    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("Test Item 1", "sans", 24), [](Ref*){
-        CCLOG("Test Item 1");
+    
+    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("play placement-1", "sans", 24), [](Ref*){
+        CCLOG("play placement-1");
+        std::string placement("placement-1");
+        sdkbox::PluginSdkboxAds::placement(placement);
     }));
-
-    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("Test Item 2", "sans", 24), [](Ref*){
-        CCLOG("Test Item 2");
+    
+    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("play placement-2", "sans", 24), [](Ref*){
+        CCLOG("play placement-2");
+        std::string placement("placement-2");
+        sdkbox::PluginSdkboxAds::placement(placement);
     }));
-
-    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("Test Item 3", "sans", 24), [](Ref*){
-        CCLOG("Test Item 3");
-    }));
-
+    
     menu->alignItemsVerticallyWithPadding(10);
     addChild(menu);
 }
 
+void HelloWorld::onAdAction(const std::string& ad_unit_id,
+                            const std::string& zone_location_place_you_name_it,
+                            sdkbox::AdActionType action_type)
+{
+    std::string action;
+    
+    switch (action_type)
+    {
+        case sdkbox::AdActionType::LOADED:
+            action = "LOADED";
+            break;
+            
+        case sdkbox::AdActionType::LOAD_FAILED:
+            action = "LOAD_FAILED";
+            break;
+            
+        case sdkbox::AdActionType::CLICKED:
+            action = "CLICKED";
+            break;
+            
+        case sdkbox::AdActionType::REWARD_STARTED:
+            action = "REWARD_STARTED";
+            break;
+            
+        case sdkbox::AdActionType::REWARD_ENDED:
+            action = "REWARD_ENDED";
+            break;
+            
+        case sdkbox::AdActionType::REWARD_CANCELED:
+            action = "REWARD_CANCELED";
+            break;
+            
+        case sdkbox::AdActionType::AD_STARTED:
+            action = "AD_STARTED";
+            break;
+            
+        case sdkbox::AdActionType::AD_CANCELED:
+            action = "AD_CANCELED";
+            break;
+            
+        case sdkbox::AdActionType::AD_ENDED:
+            action = "AD_ENDED";
+            break;
+            
+        case sdkbox::AdActionType::ADACTIONTYPE_UNKNOWN:
+            action = "ADACTIONTYPE_UNKNOWN";
+            break;
+            
+        default:
+            action = "null";
+    }
+    
+    CCLOG("onAdAction, ad_unit_id: %s, zone_location_place_you_name_it: %s, action_type: %s",
+          ad_unit_id.c_str(),
+          zone_location_place_you_name_it.c_str(),
+          action.c_str());
+}
+
+void HelloWorld::onRewardAction(const std::string& ad_unit_id,
+                                const std::string& zone_id,
+                                float reward_amount,
+                                bool reward_succeed)
+{
+    CCLOG("onRewardAction, ad_unit_id: %s, zone_id: %s, reward_amount: %0.2f, reward_succeed: %s",
+          ad_unit_id.c_str(),
+          zone_id.c_str(),
+          reward_amount,
+          reward_succeed ? "YES" : "NO");
+}
--- cpp/Classes/HelloWorldScene.h
+++ cpp/Classes/HelloWorldScene.h
@@ -2,22 +2,31 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "PluginSdkboxAds/PluginSdkboxAds.h"
 
-class HelloWorld : public cocos2d::Layer
+class HelloWorld : public cocos2d::Layer, public sdkbox::PluginSdkboxAdsListener
 {
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
     static cocos2d::Scene* createScene();
-
+    
     // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
     virtual bool init();
-
+    
     // implement the "static create()" method manually
     CREATE_FUNC(HelloWorld);
-
+    
 private:
     void createTestMenu();
-
+    
+    void onAdAction(const std::string& ad_unit_id,
+                    const std::string& zone_location_place_you_name_it,
+                    sdkbox::AdActionType action_type);
+    
+    void onRewardAction(const std::string& ad_unit_id,
+                        const std::string& zone_id,
+                        float reward_amount,
+                        bool reward_succeed);
 };
 
 #endif // __HELLOWORLD_SCENE_H__
