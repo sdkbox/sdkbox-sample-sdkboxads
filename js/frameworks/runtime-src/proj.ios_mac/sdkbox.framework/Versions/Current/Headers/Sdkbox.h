7fae0034b036343c7b4d221228fe9bb0d01dbc01
Nite Luo <darkdukey@gmail.com>
2016-11-04 14:51:48 -0700

    upgrade js to 2.3.4.6

--- js/frameworks/runtime-src/proj.ios_mac/sdkbox.framework/Versions/Current/Headers/Sdkbox.h
+++ js/frameworks/runtime-src/proj.ios_mac/sdkbox.framework/Versions/Current/Headers/Sdkbox.h
@@ -7,7 +7,7 @@
 #ifndef _CC_SDKBOX_H_
 #define _CC_SDKBOX_H_
 
-#define SDKBOX_VERSION_STR  "sdkbox V2.3.3.4"
+#define SDKBOX_VERSION_STR  "sdkbox V2.3.4.6"
 
 /**
  * Only certain compilers support __attribute__((deprecated)).
